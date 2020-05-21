51686b2bee8b7efdc24040fede57cbd4e7987370
Adam Krivka <krivka.adam@gmail.com>
2020-05-21 15:54:23 +0000

    karate-tabor verze

--- header.php
+++ header.php
@@ -17,6 +17,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Alegreya+Sans+SC|Noto+Sans&display=swap" rel="stylesheet">
 
 	<?php wp_head(); ?>
 </head>
